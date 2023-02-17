dd571b7dc2be9ca3d3d26509a159b605dc6d134e
FacundoExequielFigueroa <facundofigueroa789@gmail.com>
2023-02-17 11:44:53 -0300

    Agregado de los archivos

---
+++ JavaScript.js
@@ -0,0 +1,65 @@
+// console.log(`no bajo ni con pasta..`);
+// h1 { color: red }
+// .parrafito { ... }
+// #pid { ... }
+
+
+const h1 = document.querySelector('h1');
+
+// const input1 = document.querySelector('#calculo1');
+// const input2 = document.querySelector('#calculo2');
+// const botton = document.querySelector('#btnCalcular')
+// const resultado = document.querySelector('#result')
+
+// botton.addEventListener('click', btnOnClick)
+// function btnOnClick(){
+//     const result12= Number(input1.value) + Number(input2.value)
+//     resultado.innerHTML='El resultado es ' + result12
+//     console.log(input1.value + ' ' + input2.value)
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// -------------Clase 9 --------------------------------------------
+// const p = document.querySelector('p');
+// const parrafito = document.getElementsByClassName('parrafo');
+// const pid = document.getElementById('parrafoId');
+// console.log(input.value);
+
+// console.log({
+//     h1,
+//     p,
+//     parrafito,
+//     pid,
+//     input,
+// });
+// h1.innerHTML = "Hola <br> buenas noches"
+// h1.innerText = "Hola <br> buenas noches"
+// console.log(h1.getAttribute("class"));
+// h1.setAttribute("class", "rojo");
+// const img=document.createElement("img");
+// input.value= 456
+// img.setAttribute("src", "https://media.istockphoto.com/id/1163615699/es/foto/perro-corgi-rojo-en-gafas-de-sol-sac%C3%B3-su-bonita-cara-con-la-lengua-y-las-patas-de-la-ventana.jpg?s=612x612&w=0&k=20&c=YAvWqfvnnccIIUWPb33eHcnWoihYN1PcongzzdClAVA=" )
+// pid.append(img)
